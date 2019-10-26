ad8689f6b0c17438779230db7422851ddd8b8d2a
Spoffy <spoffeh@gmail.com>
2019-10-26 13:55:48 +0100

    Functioning basic client and basic test story

--- src/commandline/basicClient.ts
+++ src/commandline/basicClient.ts
@@ -1,8 +1,53 @@
 import * as readline from "readline";
+import {default as BuildStory} from "../formats/BasicTestStory";
+import {Choice, ListChoices, SculpturalHypertext} from "../engine/SculpturalHypertextEngine";
+import {BasicPage} from "../engine/BasicStory";
 
 let commandline = readline.createInterface({
    input: process.stdin,
    output: process.stdout,
 });
 
-commandline.write("Welcome to the command line hypertext client.");
+function askQuestion(question: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+        commandline.question(question + "\n", (answer) => {
+            resolve(answer);
+        });
+    });
+}
+
+async function main() {
+    let testStory: SculpturalHypertext<BasicPage> = BuildStory();
+
+    let choices: Choice<BasicPage>[] = ListChoices(testStory);
+
+    commandline.write("Welcome to the command line hypertext client.\n");
+    commandline.prompt();
+
+    while (choices.length > 0) {
+        let descriptions = choices.map((choice, index) =>
+            `[${index}]: ${choice.page.description}`
+        );
+
+        let question = `Please choose a page: \n${descriptions.join("\n")}`;
+
+        let answer = await askQuestion(question);
+
+        let choiceNumber = parseInt(answer, 10);
+
+        if (isNaN(choiceNumber) || choiceNumber < 0 || choiceNumber > (choices.length - 1)) {
+            commandline.write("Error: That is not a valid option\n");
+            continue;
+        }
+
+        let choice = choices[choiceNumber];
+
+        commandline.write(`\n${choice.page.content}\n\n`);
+
+        let newHypertext = choice.choose();
+
+        choices = ListChoices(newHypertext);
+    }
+}
+
+main();
---
+++ src/formats/BasicTestStory.ts
@@ -0,0 +1,25 @@
+import {Page, SculpturalHypertext, State} from "../engine/SculpturalHypertextEngine";
+import {BasicPage} from "../engine/BasicStory";
+
+let page1: BasicPage = {
+    description: "Page 1",
+    content: "This is the content of page 1",
+    conditions: [() => true],
+    effects: [(state: State) => {  state["p1visited"] = true; return state}]
+};
+
+let page2: BasicPage = {
+    description: "Page 2",
+    content: "This is the content of page 2",
+    conditions: [(state: State) => state["p1visited"]],
+    effects: []
+};
+
+function BuildStory(): SculpturalHypertext<BasicPage> {
+    return {
+        pages: [page1, page2],
+        state: {}
+    }
+}
+
+export default BuildStory;
